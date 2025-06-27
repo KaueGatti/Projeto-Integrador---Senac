dfd168fb63ceb184e283863db2adbcfc66f1b66e
KaueGatti <kauegattti@gmail.com>
2025-06-27 17:27:54 -0300

    Adicionando objeto Venda.java

---
+++ Desenvolvimento Desktop/ProjetoIntegradorDesktop/src/main/java/com/mycompany/projetointegradordesktop/Objects/Venda.java
@@ -0,0 +1,15 @@
+package com.mycompany.projetointegradordesktop.Objects;
+
+public class Venda {
+    
+    private Drogaria drogaria;
+
+    public Drogaria getDrogaria() {
+        return drogaria;
+    }
+
+    public void setDrogaria(Drogaria drogaria) {
+        this.drogaria = drogaria;
+    }
+    
+}
