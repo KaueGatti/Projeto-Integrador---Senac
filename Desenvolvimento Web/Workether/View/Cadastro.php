b0aefd0d7ce35f5aab08888c1a3f940628a08051
Kaue <kauegattti@gmail.com>
2025-09-18 17:34:13 -0300

    Terminando Wire Alta Fidelidade e Começando Desenvolvimento Web

---
+++ Desenvolvimento Web/Workether/View/Cadastro.php
@@ -0,0 +1,28 @@
+<?php ?>
+
+<!doctype html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Workether - Cadastro</title>
+    <link rel="stylesheet" href="../Style/Cadastro.css">
+</head>
+<body>
+    <main>
+        <div id="divLogo">
+            <h1 id="WORK">WORK</h1>
+            <h1 id="ETHER">ETHER</h1>
+        </div>
+        <form action="">
+            <h1>Cadastro</h1>
+            <input type="text" name="usuario" placeholder="Usuário">
+            <input type="email" name="email" placeholder="E-mail">
+            <input type="password" name="senha" placeholder="Senha">
+            <input type="password" name="confirmarSenha" placeholder="Confirmar senha">
+            <p>Já possui cadastro? <a href="" id="login">Login</a></p>
+            <button id="btnCadastrar">Cadastrar</button>
+        </form>
+    </main>
+</body>
+</html>
---
+++ Desenvolvimento Web/Workether/View/Login.php
@@ -0,0 +1,27 @@
+<?php
+?>
+
+<!doctype html>
+<html lang="pr-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Workether - Login</title>
+    <link rel="stylesheet" href="../Style/Login.css">
+</head>
+<body>
+    <main>
+        <div id="divLogo">
+            <h1 id="WORK">WORK</h1><h1 id="ETHER">ETHER</h1>
+        </div>
+        <form action="">
+            <h1>Login</h1>
+            <input type="text" name="usuario" placeholder="Usuário">
+            <input type="password" name="senha" placeholder="Senha">
+            <p>Não lembra a senha? <a href="" id="redefinirSenha">Refefinir senha</a></p>
+            <p>Não possui cadastro? <a href="" id="cadastrar">Cadastrar</a></p>
+            <button id="btnEntrar">Entrar</button>
+        </form>
+    </main>
+</body>
+</html>
