331d171521436d746f7e1dcefd4a6aa1c53eb1ba
Kaue <kauegattti@gmail.com>
2025-09-29 17:30:57 -0300

    Adicionando modais de detalhes do projeto

--- Desenvolvimento Web/Workether/DetalhesProjeto.php
+++ Desenvolvimento Web/Workether/DetalhesProjeto.php
@@ -4,7 +4,7 @@
 <section class="conteudo">
     <link rel="stylesheet" href="Style/DetalhesProjeto.css">
     <div id="divTitulo">
-        <img onclick="voltar()" src="Icones/Voltar.png" alt="">
+        <img class="btnBack" onclick="voltar()" src="Icones/Voltar.png" alt="">
         <h1 class="mb-0">Detalhes do projeto</h1>
     </div>
     <section class="sectionDetalhes" id="sectionDetalhes">
@@ -33,7 +33,7 @@
             </div>
             <div class="divChat_Equipes">
                 <button id="btnChat">Chat</button>
-                <button id="btnEquipes">Equipes</button>
+                <button onclick="interactModal('modalEquipes', 'sectionDetalhes')" id="btnEquipes">Equipes</button>
             </div>
             <div>
                 <div id="space"></div>
@@ -46,17 +46,69 @@
             </div>
         </article>
     </section>
+    <div class="modal" id="modalEquipes">
+        <div class="divTitulo">
+            <h1>Equipes</h1>
+            <img class="btnBack" onclick="interactModal('modalEquipes', 'sectionDetalhes')" src="Icones/Fechar.png"
+                 alt="">
+        </div>
+        <button onclick="interactModal('modalAdicionarEquipe', 'modalEquipes')"
+                class="btnAdicionarEquipe">+ Adicionar equipe
+        </button>
+        <section class="sectionEquipes">
+            <article class="articleEquipes">
+                <p>Equipe do Kauê</p>
+                <img src="Icones/Remover.png" alt="">
+            </article>
+        </section>
+    </div>
+    <div class="modal" id="modalAdicionarEquipe">
+        <h1 class="tituloModal">Nova Equipe</h1>
+        <div class="input-group input-nome">
+            <input type="text" id="nome" placeholder=" ">
+            <label for="nome">Nome</label>
+        </div>
+        <div class="input-group input-descricao">
+            <input type="text" id="descricao" placeholder=" ">
+            <label for="nome">Descrição</label>
+        </div>
+        <div class="input-group input-responsavel">
+            <input type="text" id="responsavel" placeholder=" ">
+            <label for="nome">Responsável</label>
+        </div>
+        <button onclick="interactModal('modalParticipantes', 'modalAdicionarEquipe')" class="btnParticipantes">Participantes</button>
+        <div class="divCancelar_Concluir">
+            <button onclick="interactModal('modalAdicionarEquipe', 'modalEquipes')" class="buttonRed" id="btnCancelar">Cancelar</button>
+            <button class="buttonGreen" id="btnConcluir">Concluir</button>
+        </div>
+    </div>
     <div class="modal" id="modalParticipantes">
         <div class="divTitulo">
             <h1>Participantes</h1>
-            <img onclick="interactModal('modalParticipantes', 'sectionDetalhes')" src="Icones/Fechar.png" alt="">
+            <img class="btnBack" onclick="interactModal('modalParticipantes', 'sectionDetalhes')"
+                 src="Icones/Fechar.png" alt="">
         </div>
-        <button class="btnAdicionarParticipantes">+ Adicionar participar</button>
+        <button onclick="interactModal('modalAdicionarParticipante', 'modalParticipantes')"
+                class="btnAdicionarParticipantes">+ Adicionar participante
+        </button>
         <section class="sectionParticipantes">
             <article class="articleParticipante">
                 <p>Kauê</p>
                 <img src="Icones/Remover.png" alt="">
             </article>
         </section>
+    </div>
+    <div class="modal" id="modalAdicionarParticipante">
+        <div class="divTitulo">
+            <h1>Adicionar participantes</h1>
+            <img class="btnBack" onclick="interactModal('modalAdicionarParticipante', 'modalParticipantes')"
+                 src="Icones/Fechar.png"
+                 alt="">
+        </div>
+        <div class="input-group">
+            <input type="text" name="usuario" placeholder=" ">
+            <label for="usuario">Usuário</label>
+        </div>
+        <button onclick="adicionarParticipante()">Adicionar</button>
     </div>
 </section>
