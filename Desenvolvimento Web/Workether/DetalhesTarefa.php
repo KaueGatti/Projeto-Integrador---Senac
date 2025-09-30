da0cf911246b1e45f54ca004368fc5e732141936
Kaue <kauegattti@gmail.com>
2025-09-30 16:45:00 -0300

    Finalizado Design e funções de todas as telas

--- Desenvolvimento Web/Workether/DetalhesTarefa.php
+++ Desenvolvimento Web/Workether/DetalhesTarefa.php
@@ -7,7 +7,7 @@
         <img onclick="voltar()" src="Icones/Voltar.png" alt="">
         <h1 class="mb-0">Detalhes da tarefa</h1>
     </div>
-    <section class="sectionDetalhes">
+    <section class="sectionDetalhes" id="sectionDetalhes">
         <article class="articleDetalhes">
             <div class="input-group" id="divInputNome">
                 <input type="text" id="nome" name="nome" placeholder=" ">
@@ -22,7 +22,7 @@
                     <input type="text" id="projeto" name="projeto" placeholder=" ">
                     <label for="projeto">Projeto</label>
                 </div>
-                <button>Comentários</button>
+                <button onclick="interactModal('modalComentarios', 'sectionDetalhes')">Comentários</button>
             </div>
             <div class="input-group divEquipe">
                 <input type="text" id="inputEquipe" name="equipe" placeholder=" ">
@@ -42,4 +42,43 @@
             </div>
         </article>
     </section>
+
+    <!-- Detalhes da Tarefa -> Comentários -->
+    <div class="modal modalComentarios" id="modalComentarios">
+        <div class="divTitulo" id="titulo">
+            <h1>Comentários</h1>
+            <img class="btnBack" onclick="interactModal('modalComentarios', 'sectionDetalhes')"
+                 src="Icones/Fechar.png" alt="">
+        </div>
+        <button onclick="interactModal('modalAdicionarComentario', 'modalComentarios')"
+                class="buttonGreen" id="btnAdicionarComentario">+ Adicionar comentário
+        </button>
+        <section class="sectionComentarios">
+            <article class="articleComentario">
+                <p class="textoComentario">Comentário...</p>
+                <div class="divInfo">
+                    <img class="imgRemover" src="Icones/Remover.png" alt="">
+                    <p class="data">20/02/2020</p>
+                    <p class="hora">18:20</p>
+                    <div class="divUser">
+                        <img src="Icones/User.png" alt="">
+                        <p>Kauê</p>
+                    </div>
+                </div>
+            </article>
+        </section>
+    </div>
+    <div class="modal modalAdicionarComentario" id="modalAdicionarComentario">
+        <h1 class="tituloModal">Novo comentário</h1>
+        <div class="textArea-group">
+            <textarea type="text" name="comentario" placeholder=" "></textArea>
+            <label for="comentario">Comentário</label>
+        </div>
+        <div class="divCancelar_Adicionar">
+            <button class="buttonRed" onclick="interactModal('modalAdicionarComentario', 'modalComentarios')">Cancelar
+            </button>
+            <button class="buttonGreen" onclick="adicionarComentario()">Adicionar</button>
+        </div>
+    </div>
+    <!-- Detalhes da Tarefa -> Comentários -->
 </section>
--- Desenvolvimento Web/Workether/NovoProjeto.php
+++ Desenvolvimento Web/Workether/NovoProjeto.php
@@ -77,7 +77,7 @@
                 class="btnAdicionarEquipe">+ Adicionar equipe
         </button>
         <section class="sectionEquipes">
-            <article class="articleEquipe">
+            <article onclick="interactModal('modalDetalhesEquipe', 'modalEquipes')" class="articleEquipe">
                 <p>Equipe do Kauê</p>
                 <img src="Icones/Remover.png" alt="">
             </article>
@@ -140,5 +140,108 @@
         <button onclick="adicionarParticipante()">Adicionar</button>
     </div>
     <!-- Novo Projeto -> Equipes -> Participantes -->
+
+    <!-- Detalhes do Projeto -> Equipes -> Detalhes da Equipe -->
+    <div class="modal modalDetalhesEquipe" id="modalDetalhesEquipe">
+        <div class="divTitulo">
+            <h1>Detalhes da equipe</h1>
+            <img class="btnBack" onclick="interactModal('modalDetalhesEquipe', 'modalEquipes')" src="Icones/Fechar.png"
+                 alt="">
+        </div>
+        <div class="input-group" id="inputNome">
+            <input type="text" name="nome" placeholder=" " readonly>
+            <label for="nome">Nome</label>
+        </div>
+        <div class="textArea-group" id="textArea-descricao">
+            <textarea type="text" name="descricao" placeholder=" " readonly></textarea>
+            <label for="descricao">Descrição</label>
+        </div>
+        <div class="grid">
+            <div class="input-group" id="inputResponsavel">
+                <input type="text" placeholder=" ">
+                <label for="">Responsável</label>
+            </div>
+            <button onclick="interactModal('modalParticipantesDetalhesEquipe', 'modalDetalhesEquipe')" class="buttonOrange">Participantes</button>
+            <button class="buttonGray">Chat</button>
+            <button onclick="loadTarefas()" class="buttonOrange">Tarefas</button>
+            <button onclick="interactModal('modalComentariosDetalhesEquipe', 'modalDetalhesEquipe')" class="buttonOrange" id="btnComentarios">Comentários</button>
+        </div>
+        <div class="divEditar_Salvar">
+            <button class="buttonBlue">Editar</button>
+            <button class="buttonGreen">Salvar</button>
+        </div>
+    </div>
+    <!-- Detalhes do Projeto -> Equipes -> Detalhes da Equipe -->
+
+    <!-- Detalhes do Projeto -> Equipes -> Detalhes da Equipe -> Participantes -->
+    <div class="modal modalParticipantes" id="modalParticipantesDetalhesEquipe">
+        <div class="divTitulo">
+            <h1>Participantes</h1>
+            <img class="btnBack" onclick="interactModal('modalParticipantesDetalhesEquipe', 'modalDetalhesEquipe')"
+                 src="Icones/Fechar.png" alt="">
+        </div>
+        <button onclick="interactModal('modalAdicionarParticipanteDetalhesEquipe', 'modalParticipantesDetalhesEquipe')"
+                class="btnAdicionarParticipantes">+ Adicionar participante
+        </button>
+        <section class="sectionParticipantes">
+            <article class="articleParticipante">
+                <p>Kauê</p>
+                <img src="Icones/Remover.png" alt="">
+            </article>
+        </section>
+    </div>
+    <div class="modal modalAdicionarParticipante" id="modalAdicionarParticipanteDetalhesEquipe">
+        <div class="divTitulo">
+            <h1>Adicionar participantes</h1>
+            <img class="btnBack" onclick="interactModal('modalAdicionarParticipanteDetalhesEquipe', 'modalParticipantesDetalhesEquipe')"
+                 src="Icones/Fechar.png"
+                 alt="">
+        </div>
+        <div class="input-group">
+            <input type="text" name="usuario" placeholder=" ">
+            <label for="usuario">Usuário</label>
+        </div>
+        <button onclick="adicionarParticipante()">Adicionar</button>
+    </div>
+    <!-- Detalhes do Projeto -> Equipes -> Detalhes da Equipe -> Participantes -->
+
+    <!-- Detalhes do Projeto -> Equipes -> Detalhes da Equipe -> Comentários -->
+    <div class="modal modalComentarios" id="modalComentariosDetalhesEquipe">
+        <div class="divTitulo">
+            <h1>Comentários</h1>
+            <img class="btnBack" onclick="interactModal('modalComentariosDetalhesEquipe', 'modalDetalhesEquipe')"
+                 src="Icones/Fechar.png" alt="">
+        </div>
+        <button onclick="interactModal('modalAdicionarComentarioDetalhesEquipe', 'modalComentariosDetalhesEquipe')"
+                class="buttonGreen" id="btnAdicionarComentario">+ Adicionar comentário
+        </button>
+        <section class="sectionComentarios">
+            <article class="articleComentario">
+                <p class="textoComentario">Comentário...</p>
+                <div class="divInfo">
+                    <img class="imgRemover" src="Icones/Remover.png" alt="">
+                    <p class="data">20/02/2020</p>
+                    <p class="hora">18:20</p>
+                    <div class="divUser">
+                        <img src="Icones/User.png" alt="">
+                        <p>Kauê</p>
+                    </div>
+                </div>
+            </article>
+        </section>
+    </div>
+    <div class="modal modalAdicionarComentario" id="modalAdicionarComentarioDetalhesEquipe">
+        <h1 class="tituloModal">Novo comentário</h1>
+        <div class="textArea-group">
+            <textarea type="text" name="comentario" placeholder=" "></textArea>
+            <label for="comentario">Comentário</label>
+        </div>
+        <div class="divCancelar_Adicionar">
+            <button class="buttonRed" onclick="interactModal('modalAdicionarComentarioDetalhesEquipe', 'modalComentariosDetalhesEquipe')">Cancelar
+            </button>
+            <button class="buttonGreen" onclick="adicionarComentario()">Adicionar</button>
+        </div>
+    </div>
+    <!-- Detalhes do Projeto -> Equipes -> Detalhes da Equipe -> Comentários -->
 
 </section>
