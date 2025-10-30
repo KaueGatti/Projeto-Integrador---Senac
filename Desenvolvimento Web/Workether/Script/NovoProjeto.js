b264d600fea11285123ceb65c5fd56b435da2677
KaueGatti <kauegattti@gmail.com>
2025-10-30 15:47:47 -0300

    refactor: separando lógica de request, validação e feedback visual dentro de initNovoProjeto

--- Desenvolvimento Web/Workether/Script/NovoProjeto.js
+++ Desenvolvimento Web/Workether/Script/NovoProjeto.js
@@ -1,65 +1,71 @@
-import {carregarComponente, interactModal, request} from "./index.js";
+import {carregarComponente, request} from "./index.js";
 import {initProjetos} from "./Projetos.js";
+import {Colors} from "./../assets/constants.js";
+
 export async function initNovoProjeto() {
 
     await carregarComponente("Loading.php");
 
     await carregarComponente("NovoProjeto.php");
 
-    let novoProjeto = {
-        nome: '',
-        descricao: '',
-        responsavel: {
-            id: '',
-            usuario: '',
-        },
-        dataConclusao: ''
-    }
-
-    let btnVoltar = document.querySelector('#btnVoltar');
-
-    btnVoltar.onclick = async () => {
-        await initProjetos();
+    let usuarioLogado = {
+        id: document.querySelector(".usuarioLogado").id,
+        usuario: document.querySelector(".usuarioLogado").textContent
     }
 
     let dataAtual = new Date().toISOString().split("T")[0];
 
-    let inputNome = document.querySelector('#inputNome');
-    let textAreaDescricao = document.querySelector('#textArea_descricao');
-    let selectResponsavel = document.querySelector('#select_responsavel');
-    let inputDataConclusao = document.querySelector('#inputDataConclusao');
-    let info = document.querySelector('#info');
+    const DOM = {
+        articleDetalhes: document.querySelector(".articleDetalhes"),
+        btnVoltar: document.querySelector('#btnVoltar'),
+        inputNome: document.querySelector('#inputNome'),
+        textAreaDescricao: document.querySelector('#textArea_descricao'),
+        selectResponsavel: document.querySelector('#select_responsavel'),
+        inputDataConclusao: document.querySelector('#inputDataConclusao'),
+        info: document.querySelector('#info'),
+        btnCancelar: document.querySelector('#btnCancelar'),
+        btnConcluir: document.querySelector('#btnConcluir'),
+    }
 
-    inputDataConclusao.min = dataAtual;
+    DOM.btnVoltar.onclick = async () => {
+        await initProjetos();
+    }
 
-    function novoProjetoValido(novoProjeto) {
+    DOM.inputDataConclusao.min = dataAtual;
 
-        if (novoProjeto.nome.replace(/ /g, '') === '') {
-            inputNome.focus();
+    function mostrarInfo(success, message) {
+        DOM.info.textContent = message;
+        DOM.info.style.color = success ? Colors.green : Colors.red;
+    }
+
+    function validarProjeto(projeto) {
+
+        if (projeto.nome.replace(/ /g, '') === '') {
+            DOM.inputNome.focus();
             return {
                 success: false,
                 message: 'Nome do projeto não pode estar vázio'
             };
         }
 
-        if (novoProjeto.descricao.replace(/ /g, '') === '') {
-            textAreaDescricao.focus();
+        if (projeto.descricao.replace(/ /g, '') === '') {
+            DOM.textAreaDescricao.focus();
             return {
                 success: false,
                 message: 'Descrição do projeto não pode estar vázio'
             };
         }
 
-        if (novoProjeto.responsavel.id === '' || novoProjeto.responsavel.usuario === '') {
-            selectResponsavel.focus();
+        if (projeto.responsavel.id === '' || projeto.responsavel.usuario === '') {
+            DOM.selectResponsavel.focus();
             return {
                 success: false,
                 message: 'Selecione um responsável para o projeto'
             };
         }
 
-        if (novoProjeto.dataConclusao < dataAtual) {
-            inputDataConclusao.focus();
+        if (projeto.dataConclusao < dataAtual) {
+            DOM.inputDataConclusao.focus();
             return {
                 success: false,
                 message: 'A data de conclusão não pode ser anterior a data atual'
@@ -73,56 +79,57 @@ export async function initNovoProjeto() {
 
     }
 
-    let btnCancelar = document.querySelector('#btnCancelar');
-    let btnConcluir = document.querySelector('#btnConcluir');
+    async function criarProjeto(projeto) {
+        let form = new FormData();
 
-    btnCancelar.onclick = () => {
-        btnVoltar.click();
+        form.append('novoProjeto[nome]', projeto.nome);
+        form.append('novoProjeto[descricao]', projeto.descricao);
+        form.append('novoProjeto[id_criador]', usuarioLogado.id);
+        form.append('novoProjeto[id_responsavel]', projeto.responsavel.id);
+        form.append('novoProjeto[dataInicialConclusao]', projeto.dataConclusao);
+
+        return await request("../API/ProjetoAPI.php", {method: "POST", body: form});
     }
 
-    btnConcluir.onclick = async function () {
+    DOM.btnCancelar.onclick = () => {
+        DOM.btnVoltar.click();
+    }
 
-        novoProjeto.nome = inputNome.value;
-        novoProjeto.descricao = textAreaDescricao.value;
-        novoProjeto.responsavel.id = selectResponsavel.options[selectResponsavel.selectedIndex].value;
-        novoProjeto.responsavel.usuario = selectResponsavel.options[selectResponsavel.selectedIndex].textContent;
-        novoProjeto.dataConclusao = inputDataConclusao.value;
+    DOM.btnConcluir.onclick = async function () {
 
-        let validacao = novoProjetoValido(novoProjeto);
-
-        if (validacao.success) {
-
-            btnCancelar.disabled = true;
-            btnConcluir.disabled = true;
-
-            let formNovoProjeto = new FormData();
-
-            formNovoProjeto.append('novoProjeto[nome]', novoProjeto.nome);
-            formNovoProjeto.append('novoProjeto[descricao]', novoProjeto.descricao);
-            formNovoProjeto.append('novoProjeto[id_criador]', selectResponsavel.options[1].value);
-            formNovoProjeto.append('novoProjeto[id_responsavel]', novoProjeto.responsavel.id);
-            formNovoProjeto.append('novoProjeto[dataInicialConclusao]', novoProjeto.dataConclusao);
-
-            try {
-                let response = await request("../API/ProjetoAPI.php", {method: "POST", body: formNovoProjeto});
-                if (response.success) {
-                    info.textContent = response.message;
-                    info.style.color = '#75CE70';
-                    setTimeout(() => {
-                        btnVoltar.click();
-                    }, 3000)
-                }
-            } catch (erro) {
-                info.textContent = erro.message;
-                info.style.color = "#E65A55";
-                btnCancelar.disabled = false;
-                btnConcluir.disabled = false;
-            }
-        } else {
-            info.style.color = "#E65A55";
-            info.textContent = validacao.message;
+        let projeto = {
+            nome: DOM.inputNome.value,
+            descricao: DOM.textAreaDescricao.value,
+            responsavel: {
+                id: DOM.selectResponsavel.options[DOM.selectResponsavel.selectedIndex].value,
+                usuario: DOM.selectResponsavel.options[DOM.selectResponsavel.selectedIndex].textContent,
+            },
+            dataConclusao: DOM.inputDataConclusao.value
         }
 
+        let validacao = validarProjeto(projeto);
 
+        if (!validacao.success) {
+            mostrarInfo(false, validacao.message);
+            return;
+        }
+
+        DOM.btnCancelar.disabled = true;
+        DOM.btnConcluir.disabled = true;
+
+        try {
+            let response = await criarProjeto(projeto);
+            if (response.success) {
+                mostrarInfo(true, response.message);
+                DOM.articleDetalhes.style.borderColor = Colors.green ;
+                setTimeout(() => {
+                    DOM.btnVoltar.click();
+                }, 2000);
+            }
+        } catch (error) {
+            mostrarInfo(false, error.message);
+            DOM.btnCancelar.disabled = false;
+            DOM.btnConcluir.disabled = false;
+        }
     }
 }
